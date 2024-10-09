b7b4586db9935ef476c8bfbb593e7ace1e3bd5ba
matheux-ss <mateussantossouza64@gmail.com>
2024-10-09 20:00:31 -0300

    Add files via upload

---
+++ exemplo.js
@@ -0,0 +1,21 @@
+document.getElementById('botao1').onclick = function () {
+	document.getElementById('mensagem').innerHTML = 'voce clicou no botao!';
+};
+
+document.getElementById('botao2') .onclick = function(){
+
+	document.getElementById('mensagem') .innerHTML='Esta é minha pagia web.';
+
+	document.write('oi meu nome e matheus');
+
+
+	document.write('<img src="th (1).jpeg">')
+   
+
+   let a,b,c; 
+   a=4;
+   b=7; 
+   c=9;
+
+
+};
